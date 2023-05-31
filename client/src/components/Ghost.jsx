d91ea42b7fd0c1d629245f41275596d00a86c868
Kalypso Homan <catcatmcgee@gmail.com>
2023-05-31 10:31:30 -0500

    Make userInput only ever have one character. Display game status

--- client/src/components/Ghost.jsx
+++ client/src/components/Ghost.jsx
@@ -7,22 +7,28 @@ const Ghost = () => {
   const [userMove, setUserMove] = useState('');
   const [game, setGame] = useState('');
 
-  const handleChange = (e, setType) => {
-    setType(e.target.value);
+  const handleChange = (e) => {
+    const input = (e.target.value);
+    setUserMove(input.slice(-1));
   };
 
-  const handleSubmit = () => {
-    axios.post('/api/ghost', userMove)
-      .then((data) => console.info('played letter submitted:\n', data))
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post('/api/ghost', {userMove})
+      .then(({data}) => {
+        setUserMove('');
+        setGame(game + data);
+      })
       .catch((err) => console.warn(err));
   };
+
 
   return (
     <div className="text wrap">
       <form>
         <h2 className="title">Play a game of ghost?</h2>
         <br />
-        <div className="rules">The rules are simple, you play one letter, then I play a letter. If the letter you play complete's a word, you loose. If the letter you play can't lead to any possible word, you loose. To make it fair, I'll tell you that we're using Meriam Webster's college dictionary. Here' I'll even let you go first.</div>
+        <div className="rules">The rules are simple. We each have five lives, one for each letter of the word "GHOST." You play a letter, then I play a letter. If the letter you play complete's a word, you loose a life. If the letter you play can't lead to any possible word, you loose a life. To make it fair, I'll tell you that we're using Meriam Webster's college dictionary as a word source. Here, I'll even let you go first.</div>
         <br />
         <div>
           <textarea className="form-control"
@@ -31,6 +37,7 @@ const Ghost = () => {
             onChange={(e) => handleChange(e, setUserMove)}/>
         </div>
         <button className="urlButton" onClick={handleSubmit}>Submit</button>
+        <h2 className="game">Current Game: {game}</h2>
       </form>
     </div>
   );
