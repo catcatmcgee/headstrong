020ddeacca7757a2b910aeb13eb6cbca7029193a
Kalypso Homan <catcatmcgee@gmail.com>
2023-06-02 06:53:16 -0500

    Delete unused winningTree import

--- server/helpers/ghost.js
+++ server/helpers/ghost.js
@@ -1,12 +1,11 @@
 const e = require('express');
 const fs = require('fs');
-const wordListPath = require('word-list');
 const path = require('path');
 console.log(__dirname)
 
-const wordArray = fs.readFileSync(wordListPath, 'utf8').split('\n');
+const wordArray = fs.readFileSync(path.resolve(__dirname, '../../words.txt'), 'utf8').split('\n');
 const wordTree = JSON.parse(fs.readFileSync('../../wordtree.json', 'utf8'));
-const winningTree = JSON.parse(fs.readFileSync('winningtree.json', 'utf8'))
+
 
 const winners = ['ahead', 'black', 'bleed', 'blimp', 'bloat', 'bluff', 'ahead', 'clarity', 'cleft', 'cliff', 'cloak', 'cluck', 'draft', 'dregs', 'drift', 'droll', 'drunk', 'dryly', 'equal', 'equator', 'frail', 'freak', 'friar', 'frock', 'fruit', 'graft', 'grenade', 'grill', 'groan', 'gruff', 'illegal', 'knack', 'knead', 'knife', 'knifing', 'knock', 'knuckle', 'lying', 'lymph', 'lynch', 'lyric', 'nylon', 'nymph', 'ozone', 'pneumatic', 'quack', 'quell', 'quick', 'quonset', 'squeamish', 'squeeze', 'squeezing', 'squelch', 'twain', 'tweed', 'twice', 'twofold', 'ulcer', 'ultra', 'vulture', 'whack', 'wheat', 'which', 'whoop', 'yield']
 
@@ -47,12 +46,12 @@ function getNextMove(game, winningTree, wordTree) {
   }
 }
 
-console.log(getNextMove('ac', winningTree, wordTree));
+
 
 const makeWordTree = (wordArray) => {
   const root = {};
   for(const word of wordArray){
-    if(word.length == 2){
+    if(word.length < 4 || word.includes('-') || word.includes("'")){
       continue;
     }
     let node = root;
@@ -71,11 +70,11 @@ const makeWordTree = (wordArray) => {
 };
 
 const writeWordTreeFile = (filePath) => {
-  const wordTree = makeWordTree(winners);
+  const wordTree = makeWordTree(wordArray);
   fs.writeFileSync(filePath, JSON.stringify(wordTree, null, 2));
   console.log(`wrote word Tree to ${filePath}`)
 }
-// writeWordTreeFile('winningtree.json');
+//writeWordTreeFile('wordtree.json');
 
 const writeSolutionsFile = (filePath) => {
   const solutions = pruneTree(wordTree);
