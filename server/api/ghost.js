3b68d923e8496c0063ab453c31cbb587600f2ba8
Kalypso Homan <catcatmcgee@gmail.com>
2023-06-02 06:51:54 -0500

    Import wordTree and winningTree, correctly return nextMove

--- server/api/ghost.js
+++ server/api/ghost.js
@@ -1,10 +1,14 @@
 const { Router } = require('express');
 const Ghost = Router();
 const { getNextMove } = require('../helpers/ghost');
+const fs = require('fs');
+const wordTree = JSON.parse(fs.readFileSync('../../wordtree.json', 'utf8'));
+const winningTree = JSON.parse(fs.readFileSync('winningtree.json', 'utf8'))
 
 Ghost.post('/', async (req, res) => {
-  const { game } = req.body;
-  const nextMove = getNextMove(game);
+  const { game }  = req.body;
+  const nextMove = getNextMove(game, winningTree, wordTree);
+  console.log('nextMove', nextMove);
   try {
     res.status(201).send(nextMove);
   } catch (err) {
