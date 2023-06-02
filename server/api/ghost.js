d2c09a0bd72725f84b402ae806ba65f88119ca29
Kalypso Homan <catcatmcgee@gmail.com>
2023-06-01 19:05:44 -0500

    Create ghost router

--- server/api/ghost.js
+++ server/api/ghost.js
@@ -1,18 +1,18 @@
 const { Router } = require('express');
 const Ghost = Router();
-const { getStory } = require('../helpers/stories');
+const { getNextMove } = require('../helpers/ghost');
 
 Ghost.post('/', async (req, res) => {
-  const { userMove } = req.body;
-  console.log('req', req);
-  console.log('userMove', userMove);
+  const { game } = req.body;
+  const nextMove = getNextMove(game);
   try {
-    res.status(201).send(userMove);
+    res.status(201).send(nextMove);
   } catch (err) {
     console.error('Failed to POST text to API:', err);
     res.sendStatus(500);
   }
-})
+});
+
 
 module.exports = {
   Ghost,
