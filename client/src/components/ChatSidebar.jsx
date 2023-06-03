6a432dbf9e95f9c129ecf72ad7a325b3dca84991
Kalypso Homan <catcatmcgee@gmail.com>
2023-06-03 10:51:14 -0500

    Add chatsidebar component

---
+++ client/src/components/ChatSidebar.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+
+const ChatSidebar = () => {
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    setNewMessage(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (newMessage.trim() !== '') {
+      setMessages([...messages, newMessage]);
+      setNewMessage('');
+    }
+  };
+
+  return (
+    <div className="chat-sidebar">
+      <div className="messages">
+        {messages.map((message, index) => (
+          <div key={index} className="message">
+            {message}
+          </div>
+        ))}
+      </div>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          class="form-control"
+          value={newMessage}
+          onChange={handleInputChange}
+          placeholder="Type your message..."
+        />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
+};
+
+export default ChatSidebar;
