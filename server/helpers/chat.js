1f35dd6bdc2635ee5c826617edbd9242bca8ee84
Kalypso Homan <catcatmcgee@gmail.com>
2023-06-04 15:08:33 -0500

    Added createMessage function

--- server/helpers/chat.js
+++ server/helpers/chat.js
@@ -1,31 +1,42 @@
 const axios = require('axios');
-const { Configuration, OpenAIApi, ChatCompletionRequestMessageRoleEnum } = require('openai');
+const { Countdown, getAllMessages, addMessage } = require('../db/index');
 const { OPENAI_TOKEN } = process.env;
 
-const configuration = new Configuration({
-  apiKey: OPENAI_TOKEN,
-});
 
-const openai = new OpenAIApi(configuration);
+const createMessage = async (gameUpdate) => {
+  try {
+    const chatMessages = getAllMessages()
+    const {text: story} = await Countdown.findOne({ where: { username: user } });
+    const initialPrompt = `You are a mean internet bully who is playing an online
+      word game. I'm going to describe the game and you're going to respond with
+      messages to your opponent. Include insults and find creative ways to bring up your opponent's
+      fear of this story: ${story}. Most messages should be about 5-7 words,
+      and messages should not exceed 20 words.`
 
-openai.createChatCompletion(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": `You are a brutal internet troll who is playing an online word game. I'm going to describe the game and you're going to respond with messages to your opponent. Include insults and find ways to bring up their fear of ${event}. Most messages should be about 5-7 words, and messages should not exceed 20 words.`},
-    {"role": "user", "content": "You just got matched with your opponent"}
-  ]
-)
+    const {data} = axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
+      model: 'text-davinci-002',
+      messages: [
+        { role: 'system', content: initialPrompt },
+        { role: 'user', content: 'You just got matched with your opponent' }
+      ].concat(chatMessages, gameUpdate),
+      temperature: 1.3,
+      max_tokens: 100
+    }, {
+      headers: {
+        'Authorization': `Bearer ${OPENAI_TOKEN}`,
+        'Content-Type': 'application/json'
+      }
+    })
 
-const getStory = async (event, task, stressors) => {
-  const response = await openai.createCompletion({
-    model: "text-davinci-002",
-    prompt: `I'm worried about an upcoming ${event} where I have to ${task}. It's stressful for this reason: ${stressors}. Please write me a story in second person perspective that describes a worst-case scenario for how this event could play out. The story should be roughly 50 words in length.`,
-    max_tokens: 200,
-    temperature: 1.30,
-  });
-  return response.data;
+    const reply = data.choices[0].message.content
+    addMessage('opponent', new Date, reply);
+    console.log(`${reply}\nSaved to database`)
+  }catch (error) {
+    console.error('Error:', error);
+  }
+
 }
 
 module.exports = {
-  getStory
+  createMessage
 }
