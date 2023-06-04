c3f8db72150a5e20871ba6c568f4fefed4c9967a
Kalypso Homan <catcatmcgee@gmail.com>
2023-06-04 16:55:43 -0500

    Fix post, delete, and put

--- server/api/chat.js
+++ server/api/chat.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const Messages = Router();
-const { getAllMessages, addMessage, deleteMessage, deleteAllMessages, editMessage } = require('../db');
+const { getMessage, getAllMessages, addMessage, deleteMessage, deleteAllMessages, editMessage } = require('../db');
 const { createMessage } = require('../helpers/chat');
 
 Messages.get('/', (req, res) => {
@@ -17,28 +17,51 @@ Messages.post('/update', (req, res) => {
 });
 
 Messages.post('/', (req, res) => {
-  return addMessage(req.body)
+  const { body: { text } } = req
+  console.log(text);
+  return addMessage('You', new Date, text)
     .then(() => res.status(201).send(null))
     .catch((err) => console.warn(err));
 });
 
-Messages.delete('/:id', (req, res) => {
-  return deleteMessage(req.params)
-    .then(() => res.status(200).send(null))
-    .catch((err) => console.warn(err));
-});
-
 Messages.delete('/all', (req, res) => {
-  return deleteAllMessages(req.params)
-    .then(() => res.status(200).send(null))
-    .catch((err) => console.warn(err));
+  try {
+    deleteAllMessages()
+    res.status(200).send(null)
+  } catch (err) {
+    console.warn(err)
+  }
 });
 
-Messages.put('/', (req, res) => {
-  console.info(req.body);
-  return editMessage(req.body)
-    .then(() => res.status(200).send(null))
-    .catch((err) => console.log(err));
+Messages.delete('/:id', async (req, res) => {
+  const {params: {id}} = req;
+  console.log('id', id);
+  try {
+    const message = await getMessage(id)
+    if (!message){
+      res.status(404).send('Message not found');
+    } else {
+      deleteMessage(id)
+      res.status(200).send(null)
+    }
+  } catch (err) {
+    console.warn(err)
+  }
+});
+
+Messages.put('/', async (req, res) => {
+  console.info('req.body', req.body);
+  try {
+    const message = await getMessage(req.body.id);
+    if(!message) {
+      res.status(404).send('Message not found');
+    } else {
+      editMessage(req.body)
+      res.status(200).send(null)
+    }
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = {
