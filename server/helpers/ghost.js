9a2982565b174aa5f4bfd97bc854dca691a87631
Kalypso Homan <catcatmcgee@gmail.com>
2023-06-05 05:48:57 -0500

    Remove unused imports

--- server/helpers/ghost.js
+++ server/helpers/ghost.js
@@ -1,11 +1,8 @@
 const e = require('express');
 const fs = require('fs');
-const path = require('path');
-console.log(__dirname)
 
 const wordArray = fs.readFileSync('words.txt', 'utf8').split('\n');
 const wordTree = JSON.parse(fs.readFileSync('wordtree.json', 'utf8'));
-
 
 const winners = ['ahead', 'black', 'bleed', 'blimp', 'bloat', 'bluff', 'clarity', 'cleft', 'cliff', 'cloak', 'cluck', 'draft', 'dregs', 'drift', 'droll', 'drunk', 'dryly', 'equal', 'equator', 'frail', 'freak', 'friar', 'frock', 'fruit', 'graft', 'grenade', 'grill', 'groan', 'gruff', 'illegal', 'knack', 'knead', 'knife', 'knifing', 'knock', 'knuckle', 'lying', 'lymph', 'lynch', 'lyric', 'nylon', 'nymph', 'ozone', 'pneumatic', 'quack', 'quell', 'quick', 'quonset', 'squeamish', 'squeeze', 'squeezing', 'squelch', 'twain', 'tweed', 'twice', 'twofold', 'ulcer', 'ultra', 'vulture', 'whack', 'wheat', 'which', 'whoop', 'xylem', 'xylophone', 'xylophonist', 'yield']
 
